6787521b9129b77cc317baacf17d52fdf8b1f648
Rob <rob@ldg.net>
2013-10-28 23:13:42 -0400

    Missed one TEMPLATE reference

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name = "TEMPLATE",
+    name = "puppet-provisioner",
     version = "0.1",
     packages = find_packages(),
     namespace_packages = ( 'aminatorplugins', ),
